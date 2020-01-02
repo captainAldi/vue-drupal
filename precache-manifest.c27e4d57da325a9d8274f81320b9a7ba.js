3931d24ba65ed82a4d4a29fbbcb4feaa201f730a
captainAldi <renaldiyulvianda@yahoo.come>
2020-01-02 17:55:37 +0700

    deploy

---
+++ precache-manifest.c27e4d57da325a9d8274f81320b9a7ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b8bf656ccf39b689b6",
+    "url": "/vue-drupal/css/chunk-vendors.5ace32da.css"
+  },
+  {
+    "revision": "67baaf6b54ab1e8187fd748c90d20eff",
+    "url": "/vue-drupal/index.html"
+  },
+  {
+    "revision": "a86a2b045b9ff35f9fda",
+    "url": "/vue-drupal/js/about.b69d5602.js"
+  },
+  {
+    "revision": "4111890b6af59b49556c",
+    "url": "/vue-drupal/js/app.cd028f6b.js"
+  },
+  {
+    "revision": "b6b8bf656ccf39b689b6",
+    "url": "/vue-drupal/js/chunk-vendors.bdc57dfa.js"
+  },
+  {
+    "revision": "8e606237a1781a6e4961d02c1d36b581",
+    "url": "/vue-drupal/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-drupal/robots.txt"
+  }
+]);
